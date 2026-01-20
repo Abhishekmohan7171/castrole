92aa5274bdb6fe7ab405fef0f61bbcc4c8f86af2
Abhishekmohan7171 <abhishekmohan7171@gmail.com>
2026-01-20 20:38:19 +0530

    fix button name

--- src/app/discover/profile.component.ts
+++ src/app/discover/profile.component.ts
@@ -445,7 +445,7 @@ import {
                           d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"
                         ></path>
                       </svg>
-                      <span>invite to chat</span>
+                      <span>chat</span>
                       }
                     </button>
                     } @if (targetUserId()) {
